c56cc25d7afd865eef05b22f1024504bc2abef53
Andreas Klapperich <andreas@MacBook003.fritz.box>
2018-05-03 22:52:08 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace DotNetCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args?.Length > 0 && (args[0] == "--help" || args[0] == "-h"))
+            {
+                string Help = $"dotnet-sysinternals {System.Reflection.Assembly.GetExecutingAssembly().GetName().Version.ToString()} starts the selected sysinternal program.";
+                Console.WriteLine(Help);
+                return;
+            }
+            var isWindows = System.Runtime.InteropServices.RuntimeInformation.IsOSPlatform(System.Runtime.InteropServices.OSPlatform.Windows);
+
+            /*
+            if (!isWindows)
+            {
+                Console.WriteLine("sysinternals works only on Windows");
+                return;
+            }
+            */
+
+            var dotnetDllPath = Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+            var sysinternalsPath = Path.Combine(dotnetDllPath, "sysinternals");
+
+            if (!Directory.Exists(sysinternalsPath))
+            {
+                try
+                {
+                    System.IO.Compression.ZipFile.ExtractToDirectory(Path.Combine(dotnetDllPath, "sysinternals.zip"), dotnetDllPath, true);
+                }
+                catch { }
+            }
+
+            var program = args.Length > 0 ? args[0] : "";
+            if (!string.IsNullOrEmpty(program))
+            {
+                if (program == "list")
+                {
+                    var param = args.Length > 1 ? args[1] : "*";
+                    foreach (var file in Directory.GetFiles(sysinternalsPath, param))
+                            Console.WriteLine(Path.GetFileName(file));
+                }
+                else
+                {
+                    try
+                    {
+                        var exe = Path.Combine(sysinternalsPath, "Cmder.exe");
+                        var dir = Directory.GetCurrentDirectory();
+                        Process.Start(exe, dir);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Can no start sysinternalsPath {program}");
+                        Console.WriteLine(ex);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("program missing");
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,86 @@
+# dotnet-sysinternals
+
+[![AppVeyor build status][appveyor-badge]](https://ci.appveyor.com/project/EifelMono/dotnet-sysinterals/branch/master)
+
+[appveyor-badge]: https://img.shields.io/appveyor/ci/EifelMono/dotnet-sysinterals/master.svg?label=appveyor&style=flat-square
+
+
+[![NuGet][main-nuget-badge]][main-nuget]
+
+[main-nuget]: https://www.nuget.org/packages/dotnet-sysinterals/
+[main-nuget-badge]: https://img.shields.io/nuget/v/dotnet-sysinterals.svg?style=flat-square&label=nuget
+
+dotnet-sysinternals starts the selected sysinternals program.
+
+This tool inculdes sysinternals Download Sysinternals Suite from February 13, 2018 from 
+https://docs.microsoft.com/en-us/sysinternals/downloads/sysinternals-suite
+
+This tool works only on windows
+
+On Windows you can insert dotnet-sysinternals also in the address line.
+
+## Install
+```
+dotnet tool install dotnet-sysinternals -g
+```
+
+## Requirements
+
+[2.1.300-preview2](https://www.microsoft.com/net/download/dotnet-core/sdk-2.1.300-preview2) .NET Core SDK or newer
+Section "Global Tools"
+
+How to check the installed dotnet version
+```
+dotnet --version
+```
+
+## Install .........
+
+### from NuGet.org
+
+```
+dotnet tool install dotnet-sysinternals -g
+```
+This downloads dotnet-sysinternals from NuGet.org
+
+### local after the build
+
+You can also clone the repo and run the cakebuiild build.ps1 or build.sh batch file.<br>
+This will install the dotnet-sysinternals from the new build without NuGet.org.
+
+## Uninstall
+
+Goto your personal folder
+
+* Windows
+```
+cd %userprofile%
+```
+* Mac and Linux
+```
+cd ~
+``` 
+Then goto the folder .dotnet/tools
+
+```
+cd .dotnet/tools
+```
+Here you find the program starter which you can delete.
+
+```
+cd .dotnet/tools/.storage
+```
+Here you find the real program which you can delete.
+
+Or use the dotnet uninstall programm
+```
+dotnet tool uninstall dotnet-sysinternals -g
+```
+
+Or use the cakebuild batch file build.sh or build.ps1 with the target uninstall
+```
+./build.ps1 -target=uninstall
+or
+./build.sh -target=uninstall
+```
+
